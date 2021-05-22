0b1d8eeaa692f51c17441196f2a2ca03207d3b63
Emre Önal <emmreonal@gmail.com>
2021-05-22 17:55:24 +0300

    Hot reloading support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import run from '@rollup/plugin-run'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js'
-  }
+  },
+  plugins: process.env.ROLLUP_WATCH && run()
 }
