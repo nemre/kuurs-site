5e16e8944aa438940e219b67d92ec1adefc3f594
Emre Önal <emmreonal@gmail.com>
2021-05-23 17:36:42 +0300

    Small changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,14 @@
 function Sidebar(props) {
-  return <Col class={classNames(props.class, props.defaultClass)}>sol</Col>
+  return (
+    <Col class={classNames(props.class, props.defaultClass)}>
+      <span>iconButton</span>
+      <Col class="flex-grow overflow-y-auto bg-white">
+        <span>courseButton</span>
+        <span>courseButton</span>
+      </Col>
+      <span>iconButton</span>
+    </Col>
+  )
 }
 
 Sidebar.defaultProps = {
