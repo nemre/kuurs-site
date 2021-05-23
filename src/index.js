f049b3f3c392870eedf8f9c0331f56b10e0f46bf
Emre Önal <emmreonal@gmail.com>
2021-05-23 15:52:35 +0300

    Small changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import render from 'preact-render-to-string'
 import express from 'express'
+import render from 'preact-render-to-string'
+
 var app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send(render(<h1>Hello</h1>))
 })
 
-app.listen(port, () => {
-  console.log(`Kuurs listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 80, () => {
+  console.log('\x1Bc')
+  console.log('Kuurs is started. Yay!')
 })
