edc76f10981c61aea671f3e191ae33f87945915a
Emre Önal <emmreonal@gmail.com>
2021-06-01 00:18:40 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,38 +2,38 @@ import * as Utils from '../utils'
 
 export default function Header(props) {
   return (
-    <div class='flex p-4'>
-      <a href='#' class='rounded-2xl flex text-red-500 p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+    <div class='flex p-4 space-x-2'>
+      <a href='#' class='rounded-2xl flex text-red-500 p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current'>
           <path d={Utils.Icons.video} />
         </svg>
         <span>Kurslar</span>
       </a>
-      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current text-blue-500'>
           <path d={Utils.Icons.helpCircle} />
         </svg>
         <span>Sorular</span>
       </a>
-      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current text-yellow-500'>
           <path d={Utils.Icons.fileText} />
         </svg>
         <span>Makaleler</span>
       </a>
-      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current text-green-500'>
           <path d={Utils.Icons.briefCase} />
         </svg>
         <span>Araçlar</span>
       </a>
-      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current text-pink-500'>
           <path d={Utils.Icons.barChart2} />
         </svg>
         <span>Sıralama</span>
       </a>
-      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-5 space-x-2'>
         <svg class='w-6 h-6 fill-current text-purple-500'>
           <path d={Utils.Icons.compass} />
         </svg>
