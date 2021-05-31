8cd1dd3cc474f5e6e6be2d7e7b9da9de6f09b1fd
Emre Önal <emmreonal@gmail.com>
2021-06-01 00:17:27 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,42 @@ import * as Utils from '../utils'
 
 export default function Header(props) {
   return (
-    <div class='flex'>
-      <a href='#' class='flex text-red-500'>
+    <div class='flex p-4'>
+      <a href='#' class='rounded-2xl flex text-red-500 p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
         <svg class='w-6 h-6 fill-current'>
           <path d={Utils.Icons.video} />
         </svg>
         <span>Kurslar</span>
       </a>
-      <a href='#' class='flex'>
-        <svg class='w-6 h-6 fill-current'>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+        <svg class='w-6 h-6 fill-current text-blue-500'>
           <path d={Utils.Icons.helpCircle} />
         </svg>
         <span>Sorular</span>
+      </a>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+        <svg class='w-6 h-6 fill-current text-yellow-500'>
+          <path d={Utils.Icons.fileText} />
+        </svg>
+        <span>Makaleler</span>
+      </a>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+        <svg class='w-6 h-6 fill-current text-green-500'>
+          <path d={Utils.Icons.briefCase} />
+        </svg>
+        <span>Araçlar</span>
+      </a>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+        <svg class='w-6 h-6 fill-current text-pink-500'>
+          <path d={Utils.Icons.barChart2} />
+        </svg>
+        <span>Sıralama</span>
+      </a>
+      <a href='#' class='rounded-2xl flex p-2 bg-black bg-opacity-0 hover:bg-opacity-10 space-x-2'>
+        <svg class='w-6 h-6 fill-current text-purple-500'>
+          <path d={Utils.Icons.compass} />
+        </svg>
+        <span>Blog</span>
       </a>
     </div>
   )
