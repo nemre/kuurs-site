540bfcee0cd3fa53803225c1a5d96aecd68907a0
Emre Önal <emmreonal@gmail.com>
2021-06-03 23:54:42 +0300

    Small changes

--- src/usr/Div.comp.js
+++ src/usr/Div.comp.js
@@ -3,24 +3,24 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+  align-items: ${(props) => props.alignItems};
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: ${(props) => props.borderRadius};
   display: ${(props) => props.display};
-  overflow: ${(props) => props.overflow};
+  flex-direction: ${(props) => props.flexDirection};
+  font-family: ${(props) => props.fontFamily};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  height: ${(props) => props.height};
+  justify-content: ${(props) => props.justifyContent};
+  line-height: ${(props) => props.lineHeight};
+  margin: ${(props) => props.margin};
   overflow-x: ${(props) => props.overflowX};
   overflow-y: ${(props) => props.overflowY};
-  flex-direction: ${(props) => props.flexDirection};
-  justify-content: ${(props) => props.justifyContent};
-  align-items: ${(props) => props.alignItems};
+  overflow: ${(props) => props.overflow};
   padding: ${(props) => props.padding};
-  margin: ${(props) => props.margin};
-  border-radius: ${(props) => props.borderRadius};
-  background-color: ${(props) => props.backgroundColor};
-  font-weight: ${(props) => props.fontWeight};
-  font-size: ${(props) => props.fontSize};
-  line-height: ${(props) => props.lineHeight};
   text-align: ${(props) => props.textAlign};
-  font-family: ${(props) => props.fontFamily};
+  width: ${(props) => props.width};
   &:hover {
     background-color: ${(props) => props.hover.backgroundColor};
   }
@@ -31,17 +31,19 @@ const Div = (props) => {
 };
 
 Div.propTypes = {
+  alignItems: PropTypes.oneOf([" ", "start", "center", "end"]),
+  backgroundColor: PropTypes.string,
+  borderRadius: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.element),
+  display: PropTypes.oneOf([" ", "flex"]),
+  flexDirection: PropTypes.oneOf([" ", "row", "column"]),
+  fontFamily: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.string,
+  height: PropTypes.string,
   hover: PropTypes.shape({
     backgroundColor: PropTypes.string,
   }),
-  children: PropTypes.arrayOf(PropTypes.element),
-  width: PropTypes.string,
-  height: PropTypes.string,
-  display: PropTypes.oneOf([" ", "flex"]),
-  overflow: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
-  overflowX: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
-  overflowY: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
-  flexDirection: PropTypes.oneOf([" ", "row", "column"]),
   justifyContent: PropTypes.oneOf([
     " ",
     "start",
@@ -51,16 +53,14 @@ Div.propTypes = {
     "space-around",
     "space-evenly",
   ]),
-  alignItems: PropTypes.oneOf([" ", "start", "center", "end"]),
-  padding: PropTypes.string,
-  margin: PropTypes.string,
-  borderRadius: PropTypes.string,
-  backgroundColor: PropTypes.string,
-  fontWeight: PropTypes.string,
-  fontSize: PropTypes.string,
   lineHeight: PropTypes.string,
+  margin: PropTypes.string,
+  overflow: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
+  overflowX: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
+  overflowY: PropTypes.oneOf([" ", "auto", "hidden", "visible", "scroll"]),
+  padding: PropTypes.string,
   textAlign: PropTypes.oneOf([" ", "left", "center", "right"]),
-  fontFamily: PropTypes.string,
+  width: PropTypes.string,
 };
 
 export default Div;
